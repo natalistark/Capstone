b9f3d6298001a3432b01a393890b290d880e9359
moon <n>
2021-05-27 12:23:13 +0200

    updated model

--- models.py
+++ models.py
@@ -21,6 +21,18 @@ class Podcasts(db.Model):
     website_link = db.Column(db.String(120))
     genres = db.Column(db.ARRAY(db.String), nullable=False)
     episodes = db.relationship('Episode', backref='podcast', lazy=True)
+   
+    #adds new entry
+    def add(self):
+        db.session.add(self)
+        db.session.commit()   
+    #updates db
+    def update(self):
+        db.session.commit()
+    #deletes entry from db
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
      return f'<Podcast {self.id} {self.name}>'
@@ -40,6 +52,18 @@ class Artist(db.Model):
     facebook_link = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
     episodes = db.relationship('Episode', backref='artist', lazy=True)  
+    
+    #adds new entry
+    def add(self):
+        db.session.add(self)
+        db.session.commit()   
+    #updates db
+    def update(self):
+        db.session.commit()
+    #deletes entry from db
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
     def __repr__(self):
      return f'<Artist {self.id} {self.name}>' 
@@ -52,6 +76,18 @@ class Episode(db.Model):
     release_time = db.Column(db.DateTime(), nullable=False)
     podcast_id = db.Column(db.Integer, db.ForeignKey('podcasts.id'), nullable=False)
     artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+    
+    #adds new entry
+    def add(self):
+        db.session.add(self)
+        db.session.commit()   
+    #updates db
+    def update(self):
+        db.session.commit()
+    #deletes entry from db
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
       
     def __repr__(self):
      return f'<Episode {self.id} {self.name}>'
