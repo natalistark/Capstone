70925bec41729f0d6250f780c7eb677bb7ada09e
moon <n>
2021-05-27 10:39:49 +0200

    added model

--- models.py
+++ models.py
@@ -1,1 +1,58 @@
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#----------------------------------------------------------------------------#
+# Models.
+#----------------------------------------------------------------------------#
+
+class Podcasts(db.Model):
+    __tablename__ = 'podcasts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    country = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    episodes = db.relationship('Episode', backref='podcast', lazy=True)
+
+    def __repr__(self):
+     return f'<Podcast {self.id} {self.name}>'
+
+
+class Artist(db.Model):
+    __tablename__ = 'artists'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    country = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    episodes = db.relationship('Episode', backref='artist', lazy=True)  
+
+    def __repr__(self):
+     return f'<Artist {self.id} {self.name}>' 
+
+
+class Episode(db.Model):
+    __tablename__ = 'episodes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    release_time = db.Column(db.DateTime(), nullable=False)
+    podcast_id = db.Column(db.Integer, db.ForeignKey('podcasts.id'), nullable=False)
+    artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), nullable=False)
+      
+    def __repr__(self):
+     return f'<Episode {self.id} {self.name}>'
+
