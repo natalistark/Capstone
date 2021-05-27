4643c1452b4e667466155925a090bbe1711d2ecf
moon <n>
2021-05-28 01:02:39 +0200

    -

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
   #initializing db
-  setup_db(app, 'postgresql://student:qwerty@localhost:5432/capstone')
+  setup_db(app, 'postgres://wfbyfhzeplqkbv:47eafc1eb9bd776cf572f1ce38a56f7d063c7d191dd1aa325ad6478f121030c2@ec2-52-0-114-209.compute-1.amazonaws.com:5432/dckvg8tq2fu9ps')
   CORS(app)
-  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://student:qwerty@localhost:5432/capstone'
+  app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wfbyfhzeplqkbv:47eafc1eb9bd776cf572f1ce38a56f7d063c7d191dd1aa325ad6478f121030c2@ec2-52-0-114-209.compute-1.amazonaws.com:5432/dckvg8tq2fu9ps'
   app.config['SECRET_KEY'] = DevelopmentConfig.SECRET_KEY
   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
