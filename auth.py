964003ab38aca9d85c98fb09f4f5ef2997121744
moon <n>
2021-05-28 02:22:01 +0200

    -

--- auth.py
+++ auth.py
@@ -7,9 +7,9 @@ import sys
 from config import DevelopmentConfig
 import os
 
-AUTH0_DOMAIN = os.environ.get(AUTH0_DOMAIN)
-ALGORITHMS = os.environ.get(ALGORITHMS)
-API_AUDIENCE = os.environ.get(API_AUDIENCE)
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 app = Flask(__name__)
 
